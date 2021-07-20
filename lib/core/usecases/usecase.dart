fa9513d460bf0d373bf195ebac9e87a8e3f35ef3
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2021-07-20 11:56:26 -0300

    update: aula 3

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,11 @@
+import 'package:clean_architecture_tdd_course/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params) {
+    print(params);
+  }
+}
+
+class NoParams extends Equatable {}
