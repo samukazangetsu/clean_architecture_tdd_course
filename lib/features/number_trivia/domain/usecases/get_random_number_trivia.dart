75fe13534de70d0416019d9a1b7cfb9da1d0537f
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2021-07-20 13:21:15 -0300

    update: aula 4

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -3,7 +3,6 @@ import 'package:clean_architecture_tdd_course/core/usecases/usecase.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
